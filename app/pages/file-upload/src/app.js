36759928cfd6d883215fa371ce96e4eca7a77e92
Manfrin <manfredd_10@hotmail.com>
2023-10-27 17:17:26 -0300

    format: project clean up

--- app/pages/file-upload/src/app.js
+++ app/pages/file-upload/src/app.js
@@ -34,25 +34,3 @@ view.configureOnFileChange((file) => {
     view.updateElapsedTime(`Process started ${time}`);
   });
 });
-
-// just to simulate the upload via html element
-async function fakeFetch() {
-  const filePath = "/videos/frag_bunny.mp4";
-
-  const response = await fetch(filePath);
-
-  // building the file like the html
-  const file = new File([await response.blob()], filePath, {
-    type: "video/mp4",
-    lastModified: Date.now(),
-  });
-
-  const event = new Event("change");
-
-  Reflect.defineProperty(event, "target", {
-    value: { files: [file] },
-  });
-
-  document.getElementById("fileUpload").dispatchEvent(event);
-}
-fakeFetch();
